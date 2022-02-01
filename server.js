d8fbde13f955c148a4c5a90a762d8ef12ed93887
Scott Everett <burningcupidxxx@gmail.com>
2022-01-31 16:36:21 -0800

    import db.json file, create post routes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // import
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const noteStorage = require("./db/db.json");
 
 // create variable for port
 const PORT = 3001;
@@ -13,10 +15,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// get requests
+
 app.get("/", (req, res) => res.send("Supppppp"));
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+})
+
+app.post("/notes", (req, res) => {
+
 })
 
 
